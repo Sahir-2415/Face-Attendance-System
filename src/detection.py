c72bff16adf6384da153f5354ec10155375d5acf
Sahir-2415 <karirsahir@gmail.com>
2026-09-12 07:17:28 +0530

    Add face embedding generation

--- src/detection.py
+++ src/detection.py
@@ -1,40 +1,72 @@
+from insightface.app import FaceAnalysis
 import mediapipe as mp
 import cv2
+import numpy as np
+app=FaceAnalysis(
+    name="buffalo_l",
+    # buffalo_l is a pretrained model for face detection and recognition
+    providers=["CPUExecutionProvider"]
+)
+app.prepare(ctx_id=0,det_size=(640,640))
 from camera import get_camera,get_frame
+
 BaseOptions=mp.tasks.BaseOptions
 FaceDetector=mp.tasks.vision.FaceDetector
 FaceDetectorOptions=mp.tasks.vision.FaceDetectorOptions
 VisionRunningMode=mp.tasks.vision.RunningMode
-options=FaceDetectorOptions(
-    base_options=BaseOptions(
-        model_asset_path="models/blaze_face_short_range.tflite"
-    ),
-    running_mode=VisionRunningMode.IMAGE
-)
-detector=FaceDetector.create_from_options(options)
+
+def create_detector():
+    options=FaceDetectorOptions(
+        base_options=BaseOptions(
+            model_asset_path="models/blaze_face_short_range.tflite"
+        ),
+        running_mode=VisionRunningMode.IMAGE
+    )
+    return FaceDetector.create_from_options(options)
+detector=create_detector()
+
+def detect_faces(detector,frame):
+    rgb=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    image=mp.Image(
+        image_format=mp.ImageFormat.SRGB,
+        data=rgb
+    )
+    # converting rgb to bgr here because mediapipe take rgb as input
+    result=detector.detect(image)
+     # now converting it to mediapipe image format as rgb is still a numpy array and media pipe needs image format
+    return result.detections
 
 cap=get_camera()
+
 while True:
     if not cap.isOpened():
         break
     frame=get_frame(cap)
     if frame is None:
         break
-    rgb=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB) 
-    image=mp.Image(image_format=mp.ImageFormat.SRGB,data=rgb)
-    # converting rgb to bgr here because mediapipe take rgb as input
-    
-    result=detector.detect(image)
-    # now converting it to mediapipe image format as rgb is still a numpy array and media pipe needs image format
+    detections=detect_faces(detector,frame)
+   
     # detection contains the info about the detected face
-    if result.detections:
-        for detection in result.detections:
+    if detections:
+        for detection in detections:
             bbox=detection.bounding_box
             x=bbox.origin_x
             y=bbox.origin_y
             width=bbox.width
             height=bbox.height
             face=frame[y:y+height,x:x+width]
+            # face_for_recognition=cv2.resize(face,(224,224))
+            embedding=app.models["recognition"].get_feat(face)
+            print(embedding.shape)
+            # this code is for face recognition and getting the embedding of the detected face
+            # if faces:
+            #     embedding=faces[0].embedding
+            #     if "saved_embedding" not in locals():
+            #         saved_embedding=embedding
+            #     similarity=np.dot(saved_embedding,embedding)/(
+            #         np.linalg.norm(saved_embedding)*np.linalg.norm(embedding)
+            #     )
+            #     print(similarity)
             cv2.rectangle(
                 frame,
                 (x,y),
