75bb0f433e640e608d4c521abc864407206a8236
Sahir-2415 <karirsahir@gmail.com>
2026-09-12 05:40:19 +0530

    Add continous face detection and cropping

--- src/detection.py
+++ src/detection.py
@@ -15,27 +15,33 @@ detector=FaceDetector.create_from_options(options)
 cap=cv2.VideoCapture(0)
 
 ret,frame=cap.read()
-rgb=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB) 
-# converting rgb to bgr here because mediapipe take rgb as input
+while True:
+    if not cap.isOpened():
+        break
+    ret,frame=cap.read()
+    if not ret:
+        break
+    rgb=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB) 
+    image=mp.Image(image_format=mp.ImageFormat.SRGB,data=rgb)
+    # converting rgb to bgr here because mediapipe take rgb as input
+    
+    result=detector.detect(image)
+    # now converting it to mediapipe image format as rgb is still a numpy array and media pipe needs image format
+    # detection contains the info about the detected face
+    if result.detections:
+        for detection in result.detections:
+            bbox=detection.bounding_box
+            x=bbox.origin_x
+            y=bbox.origin_y
+            width=bbox.width
+            height=bbox.height
+            print(x,y,width,height)
+            face=frame[y:y+height,x:x+width]
+            cv2.imshow("Face",face)
+    cv2.imshow("Camera",frame)
+    if cv2.waitKey(1)==ord('q'):
+        break
 
-# now converting it to mediapipe image format as rgb is still a numpy array and media pipe needs image format
 
-image=mp.Image(image_format=mp.ImageFormat.SRGB,data=rgb)
-
-result=detector.detect(image)
-
-# detection contains the info about the detected face
-if result.detections:
-    for detection in result.detections:
-        bbox=detection.bounding_box
-        x=bbox.origin_x
-        y=bbox.origin_y
-        width=bbox.width
-        height=bbox.height
-        print(x,y,width,height)
-        face=frame[y:y+height,x:x+width]
-        cv2.imshow("Face",face)
-        cv2.waitKey(0)
-        # this is basically to wait for a key press to close the window
-
-print(len(result.detections))
+cap.release()
+cv2.destroyAllWindows()
